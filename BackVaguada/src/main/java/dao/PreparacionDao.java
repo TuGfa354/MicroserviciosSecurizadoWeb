c2c69fc564fca076d84da07972ceee0ebbacf7e7
TuGfa354 <santica2004@hotmail.com>
2023-12-08 00:31:03 +0100

    BackFunciona pre-añadir JS

--- BackVaguada/src/main/java/dao/PreparacionDao.java
+++ BackVaguada/src/main/java/dao/PreparacionDao.java
@@ -1,9 +1,8 @@
 package dao;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import model.Preparacion;
-import model.Producto;
 
 public interface PreparacionDao {
 
@@ -11,6 +10,6 @@ public interface PreparacionDao {
 	void modificarPreparacion(Preparacion p);
 	void eliminarPreparacion(int idPreparacion);
 	Preparacion leerPreparacion(int idPreparacion);
-	ArrayList<Preparacion> listarPreparacion();
+	List<Preparacion> listarPreparacion();
 
 }
