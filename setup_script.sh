a93efc06bb965ac8cf9f938ffc0d3d5937fb77fe
TuGfa354 <santica2004@hotmail.com>
2024-03-05 17:37:40 +0100

    script

---
+++ setup_script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install necessary packages
+sudo apt-get install -y ca-certificates curl
+
+# Create directory for keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# Download Docker GPG key
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+# Set permissions for Docker GPG key
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add Docker repository to sources list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package lists again to fetch Docker packages
+sudo apt-get update
+
+# Clone the Git repository
+git clone https://github.com/TuGfa354/MicroserviciosSecurizadoWeb.git
+
+# Navigate to the cloned directory
+cd MicroserviciosSecurizadoWeb/
+
+# Build Docker image
+sudo docker build -t my-php-static-site .
+
+# Run Docker container
+sudo docker run -d -p 8080:80 my-php-static-site
